f0df8e3181785b088da6a331d003ff11671237f1
Close Su <close.su@kyperdata.com>
2018-06-21 15:47:59 +0800

    update 45

--- src/ch4/Ch45.java
+++ src/ch4/Ch45.java
@@ -3,18 +3,23 @@ package ch4;
 public class Ch45 {
 
     public Boolean checkBST(TreeNode root) {
-        if (root.left != null) {
-            if (root.left.value > root.value) {
-                return false;
-            }
-            return checkBST(root.left);
+        return checkBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    public boolean checkBST(TreeNode n, int min, int max) {
+        if (n == null) {
+            return true;
         }
-        if (root.right != null) {
-            if (root.right.value <= root.value) {
-                return false;
-            }
-            return checkBST(root.right);
+        if (n.value <= min || n.value > max) {
+            return false;
+        }
+
+        if (!checkBST(n.left, min, n.value) ||
+            !checkBST(n.right, n.value, max)  ) {
+            return false;
         }
         return true;
     }
+
+
 }
